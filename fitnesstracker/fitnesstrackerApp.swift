35be4c1a6a83f0d61d0bc6b05516b4c8dc410811
mw9 <36144660+msw009@users.noreply.github.com>
2024-09-06 19:11:19 -0500

    Initial Commit

---
+++ fitnesstracker/fitnesstrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnesstrackerApp.swift
+//  fitnesstracker
+//
+//  Created by Matthew Whigham on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct fitnesstrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
